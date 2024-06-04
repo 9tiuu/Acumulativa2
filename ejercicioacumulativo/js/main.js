aa018431d972a4bb66271a64abf0f35ba68be163
Jeremyc <88123726+9tiuu@users.noreply.github.com>
2024-06-04 02:52:19 -0400

    Avance 1.4

--- ejercicioacumulativo/js/main.js
+++ ejercicioacumulativo/js/main.js
@@ -31,11 +31,13 @@ function DestinyValidation(element) {
     };
 };
 
-function NumbersValidation(number1, number2) {
+function Number1Validation(number1, number2) {
     const ok03 = document.getElementById('ok03');
     const ok04 = document.getElementById('ok04');
     const num1 = form[number1];
-    const num2 = form[number2];
+    const num2 = form[number2]; 
+
+    // return [elementValue1, elementValue2].toString();
 
     let elementValue1 = num1.value;
     let elementValue2 = num2.value;
@@ -46,16 +48,64 @@ function NumbersValidation(number1, number2) {
         ok04.classList.add('rojo');
         ok04.classList.remove('verde');
 
-    } else if (elementValue1 < elementValue2) {
+    } else {
+        ok03.classList.add('verde');
+        ok03.classList.remove('rojo');
+    };
+
+    if (parseInt(elementValue1) < parseInt(elementValue2)) {
         ok04.classList.add('verde');
         ok04.classList.remove('rojo');
+        return elementValue1;
+        
+    } else {
+        ok04.classList.add('rojo');
+        ok04.classList.remove('verde');
+    }
+};
+
+function Number2Validation(number1, number2) {
+    const ok03 = document.getElementById('ok03');
+    const ok04 = document.getElementById('ok04');
+    const num1 = form[number1];
+    const num2 = form[number2]; 
+
+    // return [elementValue1, elementValue2].toString();
+
+    let elementValue1 = num1.value;
+    let elementValue2 = num2.value;
+
+    if (elementValue1.trim() === '' || elementValue2.trim() === '') {
+        ok03.classList.add('rojo');
+        ok03.classList.remove('verde');
+        ok04.classList.add('rojo');
+        ok04.classList.remove('verde');
 
     } else {
         ok03.classList.add('verde');
         ok03.classList.remove('rojo');
+    };
+
+    if (parseInt(elementValue1) < parseInt(elementValue2)) {
+        ok04.classList.add('verde');
+        ok04.classList.remove('rojo');
+        return elementValue2;
+        
+    } else {
         ok04.classList.add('rojo');
         ok04.classList.remove('verde');
-        return elementValue1, elementValue2;
+    }
+};
+
+function PasswordValidate(pass1, pass2 ) {
+    const password = pass1.value;
+    const passConfirm = pass2.value;
+
+    const ok05 = document.getElementById('ok05');
+    const ok06 = document.getElementById('ok06');
+
+    if (password.lenght > 7 && password.lenght < 25) {
+        
     };
 };
 
@@ -65,6 +115,12 @@ form.addEventListener('submit', function(formEvent) {
     formEvent.preventDefault();
     const name = NameValidation('nombre');
     const destiny = DestinyValidation('destino');
-    const numbers = NumbersValidation('numero1', 'numero2');
+    const number1 = Number1Validation('numero1', 'numero2');
+    const number2 = Number2Validation('numero1', 'numero2');
+
+    if (name && number1 && number2 && destiny) {
+        let formData = {nombre:name, num1:number1, num2:number2, destino:destiny }
+        console.log(formData);
+    };
 
 });
