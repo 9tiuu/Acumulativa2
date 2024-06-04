798f3d8c5a4e49c3a0dd897bd20c7f66f93c50f1
Jeremyc <88123726+9tiuu@users.noreply.github.com>
2024-06-04 05:08:12 -0400

    Avance 1.5

--- ejercicioacumulativo/js/main.js
+++ ejercicioacumulativo/js/main.js
@@ -1,126 +1,101 @@
 
 function NameValidation(element) {
-    const ok01 = document.getElementById('ok01');
+    const conditionOk01 = document.getElementById('ok01');
     const name = form[element];
     let elementValue = name.value;
 
     if (elementValue.trim() === '') {
-        ok01.classList.add('rojo');
-        ok01.classList.remove('verde');
+        RedValidate(conditionOk01);
 
     } else {
-        ok01.classList.add('verde');
-        ok01.classList.remove('rojo');
+        GreenValidate(conditionOk01);
         return elementValue;
     };
 };
 
 function DestinyValidation(element) {
-    const ok02 = document.getElementById('ok02');
+    const conditionOk02 = document.getElementById('ok02');
     const destiny = form[element];
     let elementValue = destiny.value;
 
     if (elementValue.trim() === '') {
-        ok02.classList.add('rojo');
-        ok02.classList.remove('verde');
+        RedValidate(conditionOk02)
 
     } else {
-        ok02.classList.add('verde');
-        ok02.classList.remove('rojo');
+        GreenValidate(conditionOk02);
         return elementValue;
     };
 };
 
-function Number1Validation(number1, number2) {
-    const ok03 = document.getElementById('ok03');
-    const ok04 = document.getElementById('ok04');
+function NumberValidation(number1, number2) {
+    const conditionOk03 = document.getElementById('ok03');
+    const conditionOk04 = document.getElementById('ok04');
     const num1 = form[number1];
     const num2 = form[number2]; 
-
-    // return [elementValue1, elementValue2].toString();
 
     let elementValue1 = num1.value;
     let elementValue2 = num2.value;
 
     if (elementValue1.trim() === '' || elementValue2.trim() === '') {
-        ok03.classList.add('rojo');
-        ok03.classList.remove('verde');
-        ok04.classList.add('rojo');
-        ok04.classList.remove('verde');
+        RedValidate(conditionOk03);
+        RedValidate(conditionOk04);
 
-    } else {
-        ok03.classList.add('verde');
-        ok03.classList.remove('rojo');
-    };
+    } else { GreenValidate(conditionOk03) };
 
     if (parseInt(elementValue1) < parseInt(elementValue2)) {
-        ok04.classList.add('verde');
-        ok04.classList.remove('rojo');
-        return elementValue1;
+        GreenValidate(conditionOk04);
+        return [elementValue1, elementValue2].toString();
         
-    } else {
-        ok04.classList.add('rojo');
-        ok04.classList.remove('verde');
-    }
+    } else { RedValidate(conditionOk04) }
 };
 
-function Number2Validation(number1, number2) {
-    const ok03 = document.getElementById('ok03');
-    const ok04 = document.getElementById('ok04');
-    const num1 = form[number1];
-    const num2 = form[number2]; 
+function PasswordValidate(pass1, pass2) {
+    const conditionOk05 = document.getElementById('ok05');
+    const conditionOk06 = document.getElementById('ok06');
 
-    // return [elementValue1, elementValue2].toString();
+    let formPassword = form[pass1].value;
+    let passConfirm = form[pass2].value;
 
-    let elementValue1 = num1.value;
-    let elementValue2 = num2.value;
-
-    if (elementValue1.trim() === '' || elementValue2.trim() === '') {
-        ok03.classList.add('rojo');
-        ok03.classList.remove('verde');
-        ok04.classList.add('rojo');
-        ok04.classList.remove('verde');
+    if (formPassword.trim() === '' || passConfirm.trim() === '') {
+        RedValidate(conditionOk05);
+        RedValidate(conditionOk06);
 
     } else {
-        ok03.classList.add('verde');
-        ok03.classList.remove('rojo');
+        if (formPassword === passConfirm) {
+            GreenValidate(conditionOk06);
+    
+        } else { RedValidate(conditionOk06) };
     };
 
-    if (parseInt(elementValue1) < parseInt(elementValue2)) {
-        ok04.classList.add('verde');
-        ok04.classList.remove('rojo');
-        return elementValue2;
-        
-    } else {
-        ok04.classList.add('rojo');
-        ok04.classList.remove('verde');
-    }
+    if (formPassword.length > 2 && formPassword.length < 5) {
+        GreenValidate(conditionOk05);
+        return formPassword;
+
+    } else { RedValidate(conditionOk05) };
 };
 
-function PasswordValidate(pass1, pass2 ) {
-    const password = pass1.value;
-    const passConfirm = pass2.value;
+function GreenValidate(id) {
+    id.classList.add('verde');
+    id.classList.remove('rojo');
+};
 
-    const ok05 = document.getElementById('ok05');
-    const ok06 = document.getElementById('ok06');
-
-    if (password.lenght > 7 && password.lenght < 25) {
-        
-    };
+function RedValidate(id) {
+    id.classList.add('rojo');
+    id.classList.remove('verde');
 };
 
 const form = document.forms['frm'];
 
 form.addEventListener('submit', function(formEvent) {
     formEvent.preventDefault();
-    const name = NameValidation('nombre');
-    const destiny = DestinyValidation('destino');
-    const number1 = Number1Validation('numero1', 'numero2');
-    const number2 = Number2Validation('numero1', 'numero2');
 
-    if (name && number1 && number2 && destiny) {
-        let formData = {nombre:name, num1:number1, num2:number2, destino:destiny }
+    const name = NameValidation('nombre');
+    const numbers = NumberValidation('numero1', 'numero2');
+    const passwd = PasswordValidate('password1', 'password2');
+    const destiny = DestinyValidation('destino');
+
+    if (name && numbers && passwd && destiny) {
+        let formData = {nombre:name, numeros:numbers, contraseña:passwd, destino:destiny}
         console.log(formData);
     };
-
 });
